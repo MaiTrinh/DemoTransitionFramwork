3f2d77729b8ec701b1fb539518a9ee8f5d934f9a
Mai Trinh <trinhmv92@gmail.com>
2016-08-03 14:57:29 +0700

    add 1 class, and 1 xml, edit main

--- app/src/main/java/com/example/maitrinh/demotransitionframework/Animated.java
+++ app/src/main/java/com/example/maitrinh/demotransitionframework/Animated.java
@@ -1,7 +1,12 @@
 package com.example.maitrinh.demotransitionframework;
 
+import android.view.animation.Animation;
+
 /**
  * Created by Mai Trinh on 8/3/2016.
  */
-public class Animated {
+public class Animated extends Animation{
+    public void outToLeft(){
+
+    }
 }
